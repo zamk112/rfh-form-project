8ba95c6deb29096cec58d89f6565afe8b839520b
Zamsheed Khan <zamk112@gmail.com>
2025-07-31 09:45:05 +1000

    Removing the useEffect logic from HomePage.tsx, because it is not needed.

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,32 +1,7 @@
-import { useEffect, type JSX } from "react";
+import { type JSX } from "react";
 import './HomePage.css';
 
 const Home = (): JSX.Element => {
-
-    useEffect(() => {
-        const handleHomePageClicks = (e: Event) => {
-            const target = e.target as HTMLAnchorElement;
-
-            if (target.tagName === 'A' && target.href) {
-                const href = target.getAttribute('href');
-
-                if (href === '/sync-form') {
-                    e.preventDefault();
-                    window.history.pushState({}, '', '/sync-form');
-                    window.dispatchEvent(new PopStateEvent('popstate'));
-                }
-                else if (href === '/async-form') {
-                    e.preventDefault();
-                    window.history.pushState({}, '', '/async-form');
-                    window.dispatchEvent(new PopStateEvent('popstate'));
-                }
-            }
-        };
-
-        document.addEventListener('click', handleHomePageClicks);
-
-        return () => document.removeEventListener('click', handleHomePageClicks);
-    }, []);
 
     return (
         <div className="home-container">
